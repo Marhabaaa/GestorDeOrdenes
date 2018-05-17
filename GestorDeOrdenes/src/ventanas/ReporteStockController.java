7ce46d55267a2031bb80f23eab20a2f3b02751a5
Gonzalo Pauchard Silva <gonzalo.pauchard.s@gmail.com>
2018-05-16 22:21:46 -0400

    ReporteStockController up

--- GestorDeOrdenes/src/ventanas/ReporteStockController.java
+++ GestorDeOrdenes/src/ventanas/ReporteStockController.java
@@ -1,4 +1,18 @@
 package ventanas;
 
+import application.Orden;
+import application.Pieza;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ReporteStockController {
+
+    @FXML private TableView table;
+    @FXML private TableColumn<Orden, Integer> cod;
+    @FXML private TableColumn<Pieza, String> descripcion;
+    @FXML private TableColumn<Pieza, Integer> cantidad;
+    @FXML private TableColumn<Pieza, Integer> precio;
+
+
 }
